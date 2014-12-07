70a9817a15c7006badb6188c7eab7a02a888b97b
Tomasz Rybarczyk <paluho@gmail.com>
2014-12-07 01:41:34 +0100

    Add dirty version of timestamp signer with tests

--- src/ItsItsDangerous.hs
+++ src/ItsItsDangerous.hs
@@ -1,16 +1,24 @@
 {-# LANGUAGE EmptyDataDecls, OverloadedStrings #-}
-module ItsItsDangerous (defaultSalt, defaultSeparator, rawMessage,
-                        sign, signWithSalt, signWithSeparatorAndSalt,
-                        Message, Signed, Unsigned) where
+module ItsItsDangerous
+       (attachTimestamp, defaultSalt, defaultSeparator, rawMessage, sign, signWithSalt,
+        signWithSeparatorAndSalt, signWithTimestampAndSeparator, Message,
+        extractTimestamp, Signed, Unsigned, Separator, Secret, base64encode,
+        base64decode, int2bytes, base64encode)
+       where
 
 import            Crypto.Hash.SHA1       (hash)
-import qualified  Data.ByteString
-import            Data.ByteString        (append, ByteString, spanEnd, unpack)
+import            Control.Applicative    ((<$>))
+import            Control.Monad          (mfilter)
+import qualified  Data.ByteString        as BS
+import            Data.ByteString        (append, ByteString, pack, spanEnd, breakSubstring, unpack)
 import qualified  Data.ByteString.Base64
 import            Data.ByteString.Lazy   (toStrict)
 import            Data.ByteString.Search (replace)
-import            Data.Char              (ord)
+import            Data.ByteString.UTF8   (fromString, toString)
+import            Data.Bits              (shiftL, shiftR, (.|.), (.&.))
+import            Data.Char              (ord, isDigit)
 import            Data.HMAC              (hmac_sha1)
+import            Text.Read              (readMaybe)
 
 type Secret    = ByteString
 type Separator = ByteString
@@ -41,9 +49,9 @@ base64decode :: ByteString -> Either String ByteString
 base64decode e =
   decode e
   where
-  suffix = Data.ByteString.pack $ replicate (4 - (Data.ByteString.length e `mod` 4)) (fromIntegral $ ord '=')
-  decode = Data.ByteString.Base64.decode . replaceStrict "/" "_"
-                                         . replaceStrict "+" "-"
+  suffix = BS.pack $ replicate (4 - (BS.length e `mod` 4)) (fromIntegral $ ord '=')
+  decode = Data.ByteString.Base64.decode . replaceStrict "_" "/"
+                                         . replaceStrict "-" "+"
                                          . (`append` suffix)
 
 derivateKey :: Salt -> Secret -> Key
@@ -62,7 +70,7 @@ signWithSeparatorAndSalt :: Separator -> Salt -> Secret -> ByteString -> Message
 signWithSeparatorAndSalt separator salt secret plain =
   Message $ plain `append` separator `append` m
   where
-  c = Data.ByteString.pack $ hmac_sha1 (unpack (derivateKey salt secret)) (unpack plain)
+  c = BS.pack $ hmac_sha1 (unpack (derivateKey salt secret)) (unpack plain)
   m = base64encode c
 
 signWithSalt :: Salt -> Secret -> ByteString -> Message Signed
@@ -70,3 +78,31 @@ signWithSalt = signWithSeparatorAndSalt defaultSeparator
 
 sign :: Secret -> ByteString -> Message Signed
 sign = signWithSalt defaultSalt
+
+epoch :: Integer
+epoch = 1293840000
+
+attachTimestamp :: Integer -> Separator -> ByteString -> ByteString
+attachTimestamp timestamp separator plain =
+  plain `append` separator `append` (base64encode . int2bytes . (\x -> x - epoch) $ timestamp)
+
+bytes2int :: ByteString -> Integer
+bytes2int = BS.foldl (\b a -> shiftL b 8 .|. (toInteger a)) 0
+
+int2bytes :: Integer -> ByteString
+int2bytes = pack . reverse . int2bytes'
+  where
+  int2bytes' y | y <= 0 = []
+               | otherwise = (fromInteger $ y .&. 0xff) : int2bytes' (shiftR y 8)
+
+extractTimestamp :: Separator -> ByteString -> ByteString -> Maybe Integer
+extractTimestamp separator plain plainWithSeparator = do
+  let t = BS.drop (BS.length separator + BS.length plain) $ plainWithSeparator
+  t' <- either (const Nothing) Just . base64decode $ t
+  return . (epoch +) . bytes2int $ t'
+
+signWithTimestampAndSeparator :: Integer -> Separator -> Secret -> ByteString -> Message Signed
+signWithTimestampAndSeparator timestamp separator secret plain =
+    let plain' =
+            attachTimestamp timestamp separator plain
+    in signWithSeparatorAndSalt separator defaultSalt secret plain'
--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,36 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Data.ByteString       (ByteString)
-import           Data.ByteString.UTF8  (fromString)
-import           Foreign.Python        (callObject, fromPy, getAttr,
-                                        importModule, initialize, toPy)
-import           ItsItsDangerous       (defaultSalt,
-                                        defaultSeparator, rawMessage,
-                                        sign, signWithSeparatorAndSalt)
-import           System.IO.Unsafe      (unsafePerformIO)
-import           Test.QuickCheck       (Arbitrary, arbitrary)
-import           Test.Tasty            (TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit      (testCase, (@?=))
-import           Test.Tasty.QuickCheck (testProperty)
+import Data.ByteString (ByteString)
+import Data.ByteString.UTF8 (fromString)
+import Foreign.Python
+       (callObject, fromPy, getAttr, importModule, initialize, toPy,
+        PyObject)
+import ItsItsDangerous
+       (defaultSalt, Separator, Secret, defaultSeparator, rawMessage,
+        sign, signWithSeparatorAndSalt, extractTimestamp, base64decode,
+        base64encode, signWithTimestampAndSeparator, attachTimestamp,
+        int2bytes)
+import System.IO.Unsafe (unsafePerformIO)
+import Test.QuickCheck (Arbitrary, arbitrary)
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.QuickCheck (testProperty)
 
 instance Arbitrary ByteString where
   arbitrary   = fmap fromString arbitrary
 
+data OrigSigner = TimestampSigner | Signer
+                  deriving (Show)
+
+constructSignerInstance :: OrigSigner -> Separator -> Secret -> IO PyObject
+constructSignerInstance signer separator secret = do
+    let signer' = show signer
+    initialize False
+    itsdangerous <- importModule "itsdangerous"
+    signerClass <- getAttr itsdangerous signer'
+    secret' <- toPy secret
+    salt' <- toPy defaultSalt
+    separator' <- toPy separator
+    callObject signerClass [secret', salt', separator'] []
+
+signOrigWithSigner :: OrigSigner -> Separator -> Secret -> ByteString -> ByteString
+signOrigWithSigner signer separator secret plain = unsafePerformIO $ do
+    signerInstance <- constructSignerInstance signer separator secret
+    plain' <- toPy plain
+    signMethod <- getAttr signerInstance "sign"
+    callObject signMethod [plain'] [] >>= fromPy
+
+unsingOrigWithSigner :: OrigSigner -> Separator -> Secret -> ByteString -> ByteString
+unsingOrigWithSigner signer separator secret enc = unsafePerformIO $ do
+    signerInstance <- constructSignerInstance signer separator secret
+    enc' <- toPy enc
+    unsignMethod <- getAttr signerInstance "unsign"
+    callObject unsignMethod [enc'] [] >>= fromPy
+
+signOrigWithSeparator :: Separator -> Secret -> ByteString -> ByteString
+signOrigWithSeparator separator secret plain =
+    signOrigWithSigner Signer separator secret plain
+
 signOrig :: ByteString -> ByteString -> ByteString
 signOrig = signOrigWithSeparator defaultSeparator
 
-signOrigWithSeparator :: ByteString -> ByteString -> ByteString -> ByteString
-signOrigWithSeparator separator secret plain = unsafePerformIO $ do
-  initialize False
-  itsdangerous <- importModule "itsdangerous"
-  signerClass <- getAttr itsdangerous "Signer"
-  secret' <- toPy secret
-  plain' <- toPy plain
-  salt' <- toPy defaultSalt
-  separator' <- toPy separator
-  signerInstance <- callObject signerClass [secret', salt', separator'] []
-  signMethod <- getAttr signerInstance "sign"
-  callObject signMethod [plain'] [] >>= fromPy
+timestampSignOrigWithSeparator :: Separator -> Secret -> ByteString -> ByteString
+timestampSignOrigWithSeparator separator secret plain =
+    signOrigWithSigner TimestampSigner separator secret plain
 
 testSign :: TestTree
 testSign =
@@ -39,15 +66,31 @@ testSign =
 testSignWithCustomSeparator :: TestTree
 testSignWithCustomSeparator =
     testProperty
-      "signWithSeparatorAndSalt sep defaultSeparator s p == signOrigWithSeparator sep s p" $
-        \sep s p -> let m = rawMessage $ signWithSeparatorAndSalt sep defaultSalt s p in m == signOrigWithSeparator sep s p
+        "signWithSeparatorAndSalt timestamp defaultSeparator plain == signOrigWithSeparator defaultSeparator plain" $
+        \sep secret plain ->
+             let m =
+                     rawMessage $
+                     signWithSeparatorAndSalt sep defaultSalt secret plain
+             in m ==
+                signOrigWithSeparator sep secret plain
 
+testSignWithTimestamp :: TestTree
+testSignWithTimestamp =
+  testProperty
+        "signWithTimestampAndSeparator timestamp defaultSeparator plain == timestampSignOrigWithSeparator defaultSeparator plain" $
+          \p ->
+             let secret = "secret"
+                 sep = "/"
+                 enc = timestampSignOrigWithSeparator sep secret p
+                 mt = extractTimestamp sep p $ unsingOrigWithSigner Signer sep "secret" $ enc
+             in Just enc ==
+                (mt >>= \t -> return . rawMessage $ signWithTimestampAndSeparator t sep secret p)
 
 testSignFunctions :: TestTree
 testSignFunctions = testGroup "ItsDangerous sign functions" [ testSign, testSignWithCustomSeparator ]
 
 tests :: TestTree
-tests = testGroup "ItsItsDangerous" [ testSignFunctions ]
+tests = testGroup "ItsItsDangerous" [ testSignFunctions, testSignWithCustomSeparator, testSignWithTimestamp ]
 
 main :: IO ()
 main = defaultMain tests
